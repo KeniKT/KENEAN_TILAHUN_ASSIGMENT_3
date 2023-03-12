14cb58fa5f1d96b6a89e6f9af8371a4c38319cb4
keniKT <keni232127@gmail.com>
2023-03-12 10:53:44 -0700

    phone number fixed

--- lib/presentaion/pages/profile.dart
+++ lib/presentaion/pages/profile.dart
@@ -1,11 +1,107 @@
-import 'dart:async';
+// import 'dart:async';
 
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:mobile_number/mobile_number.dart';
+
+// import '../widgets/custom_appBar.dart';
+// import '../widgets/custom_drawer.dart';
+
+// class ProfilePage extends StatefulWidget {
+//   const ProfilePage({super.key});
+
+//   @override
+//   State<ProfilePage> createState() => _ProfilePageState();
+// }
+
+// class _ProfilePageState extends State<ProfilePage> {
+//   String _mobileNumber = '';
+//   List<SimCard> _simCard = <SimCard>[];
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     MobileNumber.listenPhonePermission((isPermissionGranted) {
+//       if (isPermissionGranted) {
+//         initMobileNumberState();
+//       } else {}
+//     });
+
+//     initMobileNumberState();
+//   }
+
+//   Future<void> initMobileNumberState() async {
+//     if (!await MobileNumber.hasPhonePermission) {
+//       await MobileNumber.requestPhonePermission;
+//       return;
+//     }
+//     try {
+//       _mobileNumber = (await MobileNumber.mobileNumber)!;
+//       _simCard = (await MobileNumber.getSimCards)!;
+//     } on PlatformException catch (e) {
+//       debugPrint("Failed to get mobile number because of '${e.message}'");
+//     }
+//     if (!mounted) return;
+
+//     setState(() {});
+//   }
+
+//   Widget fillCards() {
+//     List<Widget> widgets = _simCard
+//         .map((SimCard sim) => Text(
+//             'Sim Card Number: (${sim.countryPhonePrefix}) - ${sim.number}\nCarrier Name: ${sim.carrierName}\nCountry Iso: ${sim.countryIso}\nDisplay Name: ${sim.displayName}\nSim Slot Index: ${sim.slotIndex}\n\n'))
+//         .toList();
+//     return Column(children: widgets);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//         backgroundColor: Colors.blueGrey,
+//         appBar: const CustomAppBar(),
+//         drawer: CustomDrawer(),
+//         body: Center(
+//           child: Container(
+//             padding: const EdgeInsets.only(
+//               top: 30,
+//             ),
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.center,
+//               children: [
+//                 const CircleAvatar(
+//                   backgroundImage: AssetImage('assets/profile.png'),
+//                   backgroundColor: Colors.greenAccent,
+//                   radius: 80,
+//                 ),
+//                 const SizedBox(
+//                   height: 10,
+//                 ),
+//                 const Text(
+//                   "Kenean Tilahun",
+//                   style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+//                 ),
+//                 const SizedBox(
+//                   height: 10,
+//                 ),
+//                 Text(
+//                   _mobileNumber,
+//                   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+//                 )
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+import 'package:asebeza/presentaion/widgets/custom_appBar.dart';
+import 'package:asebeza/presentaion/widgets/custom_drawer.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:mobile_number/mobile_number.dart';
-
-import '../widgets/custom_appBar.dart';
-import '../widgets/custom_drawer.dart';
+import 'package:flutter/services.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -56,7 +152,7 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+   return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: const CustomAppBar(),
